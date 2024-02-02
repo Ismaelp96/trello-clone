22d187a1f38f76df11e361f6f0112b1d3c1ed2f7
ismaelp96 <ismael6991@live.com>
2024-02-02 15:40:50 -0300

    Authentication

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -21,8 +21,9 @@ const MarketingPage = () => {
       <div
         className={cn(
           'flex items-center justify-center flex-col',
-          headingFont.className,
-        )}>
+          headingFont.className
+        )}
+      >
         <div className='mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase'>
           <Medal className='h-6 w-6 mr-2' />
           No 1 task managment
@@ -30,15 +31,16 @@ const MarketingPage = () => {
         <h1 className='text-3xl md:text-6xl text-center text-neutral-800 mb-6'>
           Taskfy helps team move
         </h1>
-        <div className='text-3xl md:text-6xl bg-gradient-to-t from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit'>
+        <div className='text-3xl md:text-6xl bg-gradient-to-t from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md  w-fit'>
           work forward.
         </div>
       </div>
       <div
         className={cn(
           'text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto',
-          textFont.className,
-        )}>
+          textFont.className
+        )}
+      >
         Collaborate, manage projects, and reach new productivity peaks. From
         high rises to the home office, the way your team works is unique -
         acccomplish it all with Taskify.
